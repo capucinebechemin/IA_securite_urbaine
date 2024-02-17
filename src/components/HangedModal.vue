373636d6238bf23a9024c5258a6a0eb4e3126eec
Aurélie Lemoine <aurelielemoine45@gmail.com>
2024-02-17 16:49:59 +0100

    add letters

--- src/components/HangedModal.vue
+++ src/components/HangedModal.vue
@@ -20,6 +20,11 @@
                         TEST
                     </div>
                 </div>
+                <div class="letters_hanged">
+                    <button v-for="a, index in alphabet" class="letter_hanged" @click="clickLetter(index)"
+                        :class="{ letter_checked_hanged: !a.clickable }" :disabled=!a.clickable>{{
+                            a.letter }}</button>
+                </div>
                 <div class="text_answer_modal" v-show="answerPage">Réponse : {{ textAnswer }}</div>
             </div>
             <div class='btn_submit_modal'>
@@ -46,9 +51,45 @@ const props = defineProps({
     textAnswer: String,
 });
 
+const alphabet = ref([
+    { "letter": "A", "clickable": true },
+    { "letter": "B", "clickable": true },
+    { "letter": "C", "clickable": true },
+    { "letter": "D", "clickable": true },
+    { "letter": "E", "clickable": true },
+    { "letter": "F", "clickable": true },
+    { "letter": "G", "clickable": true },
+    { "letter": "H", "clickable": true },
+    { "letter": "I", "clickable": true },
+    { "letter": "J", "clickable": true },
+    { "letter": "K", "clickable": true },
+    { "letter": "L", "clickable": true },
+    { "letter": "M", "clickable": true },
+    { "letter": "N", "clickable": true },
+    { "letter": "O", "clickable": true },
+    { "letter": "P", "clickable": true },
+    { "letter": "Q", "clickable": true },
+    { "letter": "R", "clickable": true },
+    { "letter": "S", "clickable": true },
+    { "letter": "T", "clickable": true },
+    { "letter": "U", "clickable": true },
+    { "letter": "V", "clickable": true },
+    { "letter": "W", "clickable": true },
+    { "letter": "X", "clickable": true },
+    { "letter": "Y", "clickable": true },
+    { "letter": "Z", "clickable": true },
+]);
+
+
 const data = ref({ questionId: null as String | null, selectedAnswer: [] as number[] });
 const selectedAnswer = ref<string>("");
 const answerPage = false;
+
+const clickLetter = (index: number) => {
+    alphabet.value[index].clickable = false;
+
+    // alphabet[index].letter
+}
 
 const submit = () => {
     store.toggleHangedModal();
@@ -74,6 +115,19 @@ const submit = () => {
     margin-left: auto;
     margin-right: auto;
     text-align: center;
+}
+
+.letters_hanged {
+    margin: auto auto;
+}
+
+.letter_hanged {
+    padding: 1vh 1vw;
+    margin: .5vh .5vw;
+}
+
+.letter_checked_hanged {
+    background-color: #638e99;
 }
 </style>
     
